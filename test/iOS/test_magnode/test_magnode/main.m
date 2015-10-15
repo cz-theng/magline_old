a2f3baf5cd50cedea16c6869cef3eea68c3340f2
C@Z <cz.theng@gmail.com>
2015-10-15 21:18:00 +0800

    delete xcode proj

--- test/iOS/test_magnode/test_magnode/main.m
+++ test/iOS/test_magnode/test_magnode/main.m
@@ -9,37 +9,33 @@
 
 #import <Foundation/Foundation.h>
 
-#include "mn_net.h"
-#include "mn_log.h"
+#include "magnode.h"
 
 void _main()
 {
-    struct mn_socket socket_hdl;
-    
-    int a = strlen(NULL);
-    
-    int ret = mn_connect("tcp://127.0.0.1:8088", &socket_hdl, 5000);
+    mn_node node;
+    int ret = mn_connect("tcp://127.0.0.1:8088", &node, 5000);
     if (ret != 0){
         LOG_E("connect error with ret %d",(int)ret);
     }
     
-    LOG_I("Connect Success!");
+    printf("Connect Success!");
     
     size_t len = 5;
-    int s = mn_send(&socket_hdl, "nimei", &len ,0);
+    int s = mn_send(&node, "nimei", len ,0);
     if (s<0) {
-        LOG_E("send error with size %d",(int)s);
+        printf("send error with size %d",(int)s);
     } else {
-        LOG_I("send with length %d",(int)len);
+        printf("send with length %d",(int)len);
     }
     
     len = 5;
     char buf[1024] = {0};
-    s = mn_recv(&socket_hdl, buf, &len, 0);
+    s = mn_recv(&node, buf, len, 0);
     if (s < 0) {
-        LOG_E("recv error with size %d", (int) len);
+        printf("recv error with size %d", (int) len);
     }
-    mn_close(&socket_hdl);
+    mn_close(&node);
 
 }
 
