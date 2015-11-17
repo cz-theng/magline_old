a3a0f6f84a76198ff4e23f49c4f641d560638db4
C@Z <cz.theng@gmail.com>
2015-11-17 21:53:41 +0800

    Add export for unity3d

---
+++ magnode/src/magnode_uinty.c
@@ -0,0 +1,54 @@
+//
+//  magnode_uinty.c
+//  magnode
+//
+//  Created by apollo on 15/11/17.
+//  Copyright © 2015年 proj-m. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "magnode_uinty.h"
+
+
+int exp_create()
+{
+    printf("exp_create");
+    return 0;
+}
+
+int exp_mn_init()
+{
+    printf("exp_mn_init");
+    return 0;
+}
+
+int exp_mn_deinit()
+{
+    printf("exp_mn_deinit");
+    return 0;
+}
+
+int exp_mn_connect()
+{
+    printf("exp_mn_connect");
+    return 0;
+}
+
+int exp_mn_send()
+{
+    printf("exp_mn_send");
+    return 0;
+}
+
+int exp_mn_recv()
+{
+    printf("exp_mn_recv");
+    return 0;
+}
+
+int exp_mn_close()
+{
+    printf("exp_mn_close");
+    return 0;
+}
